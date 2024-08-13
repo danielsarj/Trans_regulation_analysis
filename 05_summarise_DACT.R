f2831d4693601ff89511e2f8ae7d1b586c1542ae
danielsarj <dnsarj@gmail.com>
2024-08-13 13:58:04 -0500

    summarise dact results

---
+++ 05_summarise_DACT.R
@@ -0,0 +1,25 @@
+library(data.table)
+library(tidyverse)
+library(purrr) 
+read_the_file <- function(x){
+  gene_name <- substr(x, 5, nchar(x)-4)
+  d <- fread(x)
+  d <- d %>% filter(DACT_pval<(0.05/nrow(d))) %>% mutate(peri_gene=gene_name)
+  return(d)
+}
+setwd('/project/xuanyao/daniel/DACT_analysis/DACT_results/')
+
+# list of files to read
+dact_genes <- list.files('.')
+
+# use map_df to extract data from 
+# multiple files in parallel 
+sig_dact <- map_df(dact_genes, read_the_file) 
+
+# reorder columns and rows
+sig_dact <- sig_dact %>% select(peri_gene, core_gene,
+                                em_pval, mo_pval, DACT_pval) %>%
+  arrange(DACT_pval) %>% filter(peri_gene!=core_gene)
+
+# save results
+fwrite(sig_dact, '../DACT_sig_results.txt', quote=F, sep=' ')
